1232a4222cd5c4320b36637002e71d56fa902e00
Simone Perini <39993316+CoffeePerry@users.noreply.github.com>
2020-07-22 20:03:44 +0200

    Update user.py

--- mercury/services/user.py
+++ mercury/services/user.py
@@ -145,6 +145,6 @@ def register_user(username):
     if not username:
         print('Please, enter an unique Username.')
     response = input(f'Are you really sure to register new user {username}? [Y/n]:')
-    if response == 'Y':
+    if response.lower() == 'y':
         user = insert_user(username)
         print(f'{user} successfully created')
