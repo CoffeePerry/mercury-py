b3da98e76f5a6e916e115d510a7ba657250e74a0
Simone Perini <39993316+CoffeePerry@users.noreply.github.com>
2022-10-17 20:20:45 +0200

    Update custom_exceptions.py

--- mercury/services/custom_exceptions.py
+++ mercury/services/custom_exceptions.py
@@ -7,8 +7,7 @@ class MethodVersionNotFound(HTTPException):
     """*400* `Method Version Not Found` (Bad Request)
 
     Raise if the browser request method through an invalid method version ('Accept-Version').
-    (Raise if the browser sends something to the application the application
-    or server cannot handle).
+    (Raise if the browser sends something to the application or server cannot handle).
     """
 
     code = 400
